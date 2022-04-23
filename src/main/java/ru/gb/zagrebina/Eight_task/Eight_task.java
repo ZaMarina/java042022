e789064815381da47d26bea87f09edb58b8f56d9
Marina <marinamingaleva92@gmail.com>
2022-04-24 00:33:08 +0700

    My tasks  for homework3 tack 8

--- src/main/java/ru/gb/zagrebina/Eight_task/Eight_task.java
+++ src/main/java/ru/gb/zagrebina/Eight_task/Eight_task.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Eight_task {
     public static void main(String[] args) {
         int[] task8 = {1, 5, 6, 8, 4};
-        mixtArray(task8, 3);
+        mixtArray(task8, -3);
 
     }
 
@@ -21,7 +21,7 @@ public class Eight_task {
             }
             System.out.println(Arrays.toString(array8));
         } else {
-            for (int j = number; j <= -1; j++) {
+            for (int j = number; j < 0; j++) {
                 int element = array8[0];
                 for (int i = 1; i < array8.length; i++) {
                     array8[i - 1] = array8[i];
@@ -29,6 +29,7 @@ public class Eight_task {
                 array8[array8.length - 1] = element;
 
             }
+            System.out.println(Arrays.toString(array8));
         }
     }
 }

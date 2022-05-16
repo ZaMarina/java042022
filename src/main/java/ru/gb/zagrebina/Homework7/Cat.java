c47d4480347174176330cd1868d59e7a71cba727
Marina <marinamingaleva92@gmail.com>
2022-05-16 15:15:16 +0700

    "cat food".It works

---
+++ src/main/java/ru/gb/zagrebina/Homework7/Cat.java
@@ -0,0 +1,40 @@
+package ru.gb.zagrebina.Homework7;
+
+public class Cat {
+    private String name;
+       private int appetite;//(сколько единиц еды он потребляет за приём пищи
+    private boolean satiety;//сытость
+
+    public Cat(String name, int appetite, boolean satiety) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = satiety;
+    }
+
+    public void eat(Plate p){//в скобках как int a
+p.decreaseFood(appetite);
+
+
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", appetite=" + appetite +
+                ", satiety=" + satiety +
+                '}';
+    }
+}
